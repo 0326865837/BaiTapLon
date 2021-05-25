84e0c16398963b49a29bfe4660a547e881855332
Your Name <you@example.com>
2021-05-25 19:34:30 +0700

    update dal

--- BaiTapLon/DAL/SqlHelper.cs
+++ BaiTapLon/DAL/SqlHelper.cs
@@ -10,9 +10,8 @@ namespace DAL
 {
     public class SqlHelper
     {
-        private static readonly string sql_con=@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=quanlyhocsinhcap2;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private static readonly string sql_con= @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=QLHStieuhoc;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         public static SqlConnection connection = null;
-        //  lenh sql tra ve 1 bang
 
         public static int ExcuteNonQuery(string sql, params SqlParameter[] parameters)
         {
@@ -35,29 +34,7 @@ namespace DAL
                 if (connection != null) connection.Close();
             }
         }
-        public static int ExcuteNonQuery(string sql, bool isProc = false, params SqlParameter[] parameters)
-        {
-            SqlConnection connection = null;
-            try
-            {
-                connection = new SqlConnection(sql_con);
-                connection.Open();
-                SqlCommand command = new SqlCommand(sql, connection);
-                if (isProc)
-                    command.CommandType = CommandType.StoredProcedure;
-                if (parameters != null)
-                    command.Parameters.AddRange(parameters);
-                return command.ExecuteNonQuery();
-            }
-            catch (SqlException ex)
-            {
-                throw new Exception("Lỗi truy cập cơ sở dữ liệu" + ex.Message);
-            }
-            finally
-            {
-                if (connection != null) connection.Close();
-            }
-        }
+
         public static SqlDataReader ExcuteReader(string sql, SqlParameter[] parameters)
         {
             connection = null;
@@ -137,6 +114,31 @@ namespace DAL
                     connection.Close();
             }
             return isSuccess;
+        }
+
+        public static DataTable GetTable(string sql)
+        {
+            SqlConnection connection = null;
+            DataTable dt = new DataTable();
+            try
+            {
+                connection = new SqlConnection(sql_con);
+                connection.Open();
+
+                SqlDataAdapter da = new SqlDataAdapter(sql, connection);
+                da.Fill(dt);
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+            
+
+            return dt;
         }
     }
 }
